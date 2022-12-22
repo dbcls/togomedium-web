4fd5e8c971a6b7040fb7a59cd09f06cf6b0b4cde
satoshionoda <satoshionoda@gmail.com>
2022-12-22 16:27:15 +0900

    ファイル構造基礎

---
+++ webpack.config.ts
@@ -0,0 +1,82 @@
+import { makeConfig } from "./webpack/makeConfig";
+import { join } from "path";
+//
+// 本ファイル内のパスの記述は原則マシン内の絶対パス基準で統一する
+// そのため `process.cwd()` を最初に定義しておくと便利
+const CWD = process.cwd();
+const DIR_SRC = join(CWD, "src");
+const DIR_PUBLIC = join(CWD, "public");
+const DIR_ASSETS = join(DIR_PUBLIC, "assets");
+//
+export default makeConfig({
+  output: DIR_PUBLIC,
+  server: {
+    root: DIR_PUBLIC,
+    watch: `${DIR_SRC}/**/*.*`,
+    browserSync: {
+      middleware: (req, res, next) => {
+        if (!req.url) return next();
+        //
+        switch (true) {
+          case /^\/medium\/.+/.test(req.url):
+            req.url = "/medium/detail.html";
+            break;
+          case /^\/organism\/.+/.test(req.url):
+            req.url = "/organism/detail.html";
+            break;
+          case /^\/component\/.+/.test(req.url):
+            req.url = "/component/detail.html";
+            break;
+        }
+        return next();
+      },
+    },
+  },
+  pug: {
+    src: join(DIR_SRC, "pug"),
+    dest: join(DIR_PUBLIC),
+    data: ["_include/settings.js", "_include/meta.js"],
+    files: {
+      "index.html": "home.pug",
+      "medium/index.html": "medium-list.pug",
+      "medium/detail.html": "medium-detail.pug",
+      "component/index.html": "component-list.pug",
+      "component/detail.html": "component-detail.pug",
+      "organism/index.html": "organism-list.pug",
+      "organism/detail.html": "organism-detail.pug",
+      "find-media-by-components.html": "find-media-by-components.pug",
+      "find-media-by-taxonomic-tree.html": "find-media-by-taxonomic-tree.pug",
+      "find-media-by-organism-phenotype.html": "find-media-by-organism-phenotype.pug",
+      "find-media-by-relavance.html": "find-media-by-relavance.pug",
+      "compare-media.html": "compare-media.pug",
+      "compare-media-of-organisms.html": "compare-media-of-organisms.pug",
+      "compare-media-width-kegg-tree-alignment.html": "compare-media-width-kegg-tree-alignment.pug",
+      "about.html": "about.pug",
+      "statistics.html": "statistics.pug",
+    },
+  },
+  sass: {
+    src: join(DIR_SRC, "sass"),
+    dest: join(DIR_ASSETS, "css"),
+    files: {
+      "test01.css": "test01.scss",
+    },
+  },
+  ts: {
+    src: join(DIR_SRC, "ts"),
+    dest: join(DIR_ASSETS, "js"),
+    files: {
+      "main.js": "main.ts",
+    },
+  },
+  copy: [
+    {
+      from: join(DIR_SRC, "assets/libs"),
+      to: join(DIR_ASSETS, "libs"),
+    },
+    {
+      from: join(DIR_SRC, "assets/img"),
+      to: join(DIR_ASSETS, "images"),
+    },
+  ],
+});
---
+++ webpack/makeConfig.ts
@@ -0,0 +1,321 @@
+import { CompileConf, ConfigProps, PugConf } from "./types";
+import { join, relative } from "path";
+import { inspect } from "util";
+import type {
+  Configuration,
+  EntryObject,
+  ModuleOptions,
+  RuleSetRule,
+  WebpackPluginInstance,
+} from "webpack";
+import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
+const ansis = require("ansis");
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const PugPlugin = require("pug-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const LiveReloadPlugin = require("webpack-livereload-plugin");
+const WebpackBar = require("webpackbar");
+type Output = Configuration["output"];
+type Entry = Configuration["entry"];
+type Optimization = NonNullable<Configuration["optimization"]>;
+
+const IS_DEVELOP = process.env.MODE === "development";
+const IS_BROWSER_SYNC = process.env.SERVER === "browserSync";
+const IS_DEV_SERVER = process.env.SERVER === "webpack";
+const IS_LOG_WEBPACK = process.env.LOG === "webpack";
+const IS_SERVE = IS_DEV_SERVER || IS_BROWSER_SYNC;
+const mode = IS_DEVELOP ? "development" : "production";
+console.log(ansis.blue("making webpack config"));
+console.log(ansis.blue("mode:"), ansis.bgMagenta(mode));
+if (IS_SERVE) {
+  console.log(ansis.blue("server:"), ansis.bgMagenta(process.env.SERVER));
+}
+//
+export const makeConfig = (primary: ConfigProps, ...rest: ConfigProps[]): Configuration => {
+  if (rest.length === 0) {
+    return makeSingleConfig(primary);
+  } else {
+    const allProps: ConfigProps[] = [primary, ...rest];
+    allProps.forEach((props: ConfigProps) => {
+      if (!props.id) throw new Error("multiple configs require id at each ");
+    });
+    //
+    const configID = process.env.CONFIG;
+    if (!configID) {
+      console.log(ansis.red(`no config id found at process, so building`), ansis.bgRed(primary.id));
+      return makeSingleConfig(primary);
+    }
+    //
+    console.log(ansis.blue("config:"), ansis.bgMagenta(configID));
+    const targetProps: ConfigProps | undefined = allProps.find((config) => config.id === configID);
+    if (!targetProps) throw new Error(`not found config: ${configID}`);
+    return makeSingleConfig(targetProps);
+  }
+};
+//
+const makeSingleConfig = (props: ConfigProps): Configuration => {
+  const result: Configuration = {
+    watch: IS_BROWSER_SYNC,
+    context: process.cwd(),
+    mode,
+    devtool: IS_DEVELOP ? "inline-source-map" : false,
+    entry: makeEntry(props),
+    plugins: makePlugins(props),
+    module: makeModule(props),
+    devServer: IS_DEV_SERVER ? makeDevServer(props) : undefined,
+    output: makeOutput(props.output),
+    optimization: makeOptimization(props.optimization),
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".json"],
+    },
+    stats: {
+      cachedAssets: false,
+      cachedModules: false,
+      runtimeModules: false,
+      dependentModules: false,
+      assetsSpace: 9999,
+      modulesSpace: 0,
+    },
+  };
+  if (IS_LOG_WEBPACK) {
+    console.log(inspect({ ...result, plugins: "@@OMITTED@@" }, false, null, true));
+  }
+  return result;
+};
+
+const makeDevServer = ({ server }: ConfigProps): DevServerConfiguration => {
+  const { port, root, watch } = server;
+  return {
+    port: port ?? 9000,
+    open: false,
+    static: {
+      directory: root,
+    },
+    watchFiles: {
+      paths: [watch],
+      options: {
+        usePolling: true,
+      },
+    },
+  };
+};
+
+const makeEntry = ({ pug, less, sass, ts, output }: ConfigProps): Entry => {
+  let result: Entry = {};
+  if (pug) result = { ...result, ...makeEntryFromConf(pug, output) };
+  if (less) result = { ...result, ...makeEntryFromConf(less, output) };
+  if (sass) result = { ...result, ...makeEntryFromConf(sass, output) };
+  if (ts) result = { ...result, ...makeEntryFromConf(ts, output) };
+
+  return result;
+};
+
+const makeEntryFromConf = ({ files, dest, src }: CompileConf, output: string): EntryObject => {
+  const result: Entry = {};
+  const relativeSrc = relative(process.cwd(), src);
+  const relativeDest = relative(output, dest);
+  Object.entries(files).forEach(([key, value]) => {
+    const filename = join(relativeDest, key);
+    const importStr = `./${join(relativeSrc, value)}`;
+    result[filename] = {
+      filename,
+      import: importStr,
+    };
+  });
+  return result;
+};
+
+const makeOutput = (path: string): Output => {
+  if (path === process.cwd()) {
+    throw new Error("DO NOT SET output path as cwd");
+  }
+  return {
+    path,
+    publicPath: "/",
+  };
+};
+
+const makePlugins = ({ copy, server, clean, output }: ConfigProps): WebpackPluginInstance[] => {
+  const { root, port } = server;
+  //
+  const browserSync = IS_BROWSER_SYNC
+    ? new BrowserSyncPlugin({
+        host: "localhost",
+        port: port ?? 9000,
+        server: { baseDir: [root] },
+        open: false,
+        ...server.browserSync,
+      })
+    : undefined;
+  //
+  const liveReload = !!server.liveReloadPlugin
+    ? new LiveReloadPlugin({ delay: 100, useCompilationHash: false })
+    : undefined;
+
+  //
+  return [
+    browserSync,
+    liveReload,
+    new CleanWebpackPlugin({
+      verbose: clean?.verbose === undefined ? false : clean.verbose,
+      dry: clean?.dry === undefined ? false : clean.dry,
+      cleanOnceBeforeBuildPatterns: makeCleanLocationPattern(clean, output),
+    }),
+    new PugPlugin({
+      pretty: true,
+      modules: [PugPlugin.extractCss({ test: /\.(css|less|sass|scss)$/ })],
+    }),
+    new CopyPlugin({
+      patterns: copy,
+    }),
+    new WebpackBar(),
+  ].filter((item) => !!item);
+};
+
+const loadPugData = ({ data, src }: PugConf): any => {
+  if (!data) return {};
+  //
+  let result = {};
+  data.forEach((str) => {
+    const path = join(src, str);
+    const data = require(path);
+    result = { ...result, ...data };
+  });
+  return result;
+};
+
+const makeModule = ({ pug, less, sass, ts }: ConfigProps): ModuleOptions => {
+  const rules: ModuleOptions["rules"] = [];
+  pug ? rules.push(makePugLoadRule(pug)) : "";
+  less ? rules.push(makeLessLoadRule()) : "";
+  sass ? rules.push(makeSassLoadRule()) : "";
+  ts ? rules.push(makeTsLoadRule()) : "";
+  return { rules };
+};
+
+const makeTsLoadRule = (): RuleSetRule => {
+  return {
+    test: /\.(ts|tsx)$/,
+    use: [
+      {
+        loader: "ts-loader",
+      },
+    ],
+  };
+};
+
+const makePugLoadRule = (pug: PugConf): RuleSetRule => {
+  return {
+    test: /\.pug$/,
+    use: [
+      {
+        loader: PugPlugin.loader,
+        options: {
+          method: "render",
+          data: {
+            hash: IS_DEVELOP ? "" : "?" + new Date().valueOf().toString(),
+            ...loadPugData(pug),
+          },
+        },
+      },
+    ],
+  };
+};
+
+const makeSassLoadRule = (): RuleSetRule => {
+  return {
+    test: /\.(sass|scss)$/,
+    use: [
+      {
+        loader: "css-loader",
+        options: {
+          url: false,
+        },
+      },
+      {
+        loader: "sass-loader",
+      },
+    ],
+  };
+};
+
+const makeLessLoadRule = (): RuleSetRule => {
+  return {
+    test: /\.(less)$/,
+    use: [
+      {
+        loader: "css-loader",
+        options: {
+          url: false,
+        },
+      },
+      {
+        loader: "less-loader",
+        options: {
+          lessOptions: {
+            relativeUrls: false,
+          },
+        },
+      },
+    ],
+  };
+};
+
+const makeOptimization = (optimization: ConfigProps["optimization"]): Optimization => {
+  const minimizer: Optimization["minimizer"] = [];
+  if (!(optimization && optimization.minifyJs === false)) {
+    minimizer.push(new TerserPlugin());
+  }
+  if (!(optimization && optimization.minifyCss === false)) {
+    minimizer.push(new CssMinimizerPlugin());
+  }
+  if (!(optimization && optimization.minifyImages === false)) {
+    let jpgQuality = 80;
+    let pngQuality = [0.6, 0.8];
+    if (typeof optimization?.minifyImages === "object") {
+      if (optimization.minifyImages.jpg) {
+        jpgQuality = optimization.minifyImages.jpg;
+      }
+      if (optimization.minifyImages.png) {
+        pngQuality = optimization.minifyImages.png;
+      }
+    }
+    minimizer.push(
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              ["gifsicle", { interlaced: true }],
+              ["mozjpeg", { quality: jpgQuality }],
+              ["pngquant", { quality: pngQuality }],
+            ],
+          },
+        },
+      })
+    );
+  }
+  return { minimizer };
+};
+
+const makeCleanLocationPattern = (
+  clean: ConfigProps["clean"] | undefined,
+  output: string
+): string[] | undefined => {
+  if (!clean) return undefined;
+  if (clean.run === false) return [];
+
+  clean.paths?.forEach((path) => {
+    const relativePath = relative(output, path);
+    const test = /^\.\./.test(relativePath);
+    if (test) {
+      throw new Error(`Should not delete outside of the output directory ${path}`);
+    }
+  });
+
+  return clean.paths;
+};
