b10c9601b1f958e7648685b1fa843e99a43c099d
Satoshi Onoda <satoshionoda@gmail.com>
2024-03-11 12:02:27 +0900

    wip

--- src/ts/main/stanza/initMediumDetail.ts
+++ src/ts/main/stanza/initMediumDetail.ts
@@ -5,8 +5,9 @@ import { makePageTitle } from "../../utils/makePageTitle";
 
 export const initMediumDetail = async (id: string) => {
   const data = await getData("gmdb_medium_by_gmid", `gm_id=${id}`);
-  const hasName: boolean = data?.meta?.name !== "" && !!data?.meta?.name;
-  const name = hasName ? data.meta.name : id;
+  const hasName: boolean =
+    data?.meta?.name !== "" && data?.meta?.name !== "(Unnamed medium)" && !!data?.meta?.name;
+  const name = hasName ? `[${id}] ${data.meta.name}` : `[${id}] `;
   qs("title")!.innerHTML = makePageTitle(name);
   //
   const info = qs("#info")!;
