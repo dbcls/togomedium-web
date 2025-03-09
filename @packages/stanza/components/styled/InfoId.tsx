3228d8f15d4763315627d75dc85b8ed4a38b514c
satoshionoda <satoshionoda@yohak.design>
2025-03-09 13:15:14 +0900

    Enhance layout of InfoId component by aligning items centrally

--- @packages/stanza/components/styled/InfoId.tsx
+++ @packages/stanza/components/styled/InfoId.tsx
@@ -3,6 +3,7 @@ import { THEME } from "%stanza/styles/theme";
 
 export const InfoId = styled("div")({
   display: "flex",
+  alignItems: "center",
   "& span": {
     fontWeight: 300,
     fontSize: 16,
@@ -11,6 +12,7 @@ export const InfoId = styled("div")({
     display: "flex",
     marginLeft: 20,
     gap: 4,
+    alignItems: "center",
   },
 
   a: {
