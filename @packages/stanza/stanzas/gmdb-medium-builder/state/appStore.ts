ea4417038ac036b22468089bcc498881f2be8bb6
satoshionoda <satoshionoda@yohak.design>
2026-03-12 22:25:45 +0900

    feat: Add `createInitialState` utility for preloading state and update Redux store and `addSolutionThunk` accordingly

--- @packages/stanza/stanzas/gmdb-medium-builder/state/appStore.ts
+++ @packages/stanza/stanzas/gmdb-medium-builder/state/appStore.ts
@@ -1,6 +1,7 @@
 import { documentReducer } from "%stanza/stanzas/gmdb-medium-builder/state/slices/document";
 import { componentRowModelReducer } from "%stanza/stanzas/gmdb-medium-builder/state/slices/entities/ComponentRowModelSlice";
 import { solutionBlockModelReducer } from "%stanza/stanzas/gmdb-medium-builder/state/slices/entities/SolutionBlockModelSlice";
+import { createInitialState } from "%stanza/stanzas/gmdb-medium-builder/state/utils/createInitialState";
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
@@ -12,6 +13,7 @@ export const appStore = configureStore({
     }),
     document: documentReducer,
   },
+  preloadedState: createInitialState(),
 });
 
 export type AppState = ReturnType<typeof appStore.getState>;
--- @packages/stanza/stanzas/gmdb-medium-builder/state/thunks/addSolutionThunk.ts
+++ @packages/stanza/stanzas/gmdb-medium-builder/state/thunks/addSolutionThunk.ts
@@ -1,13 +1,22 @@
 import { AppDispatch } from "%stanza/stanzas/gmdb-medium-builder/state/appStore";
 import { DocumentActions } from "%stanza/stanzas/gmdb-medium-builder/state/slices/document";
 import {
+  ComponentRowModelActions,
+  createBlankComponentRow,
+} from "%stanza/stanzas/gmdb-medium-builder/state/slices/entities/ComponentRowModelSlice";
+import {
   createBlankSolutionBlock,
   SolutionBlockModelActions,
 } from "%stanza/stanzas/gmdb-medium-builder/state/slices/entities/SolutionBlockModelSlice";
 
 export const addSolutionThunk = () => {
   return (dispatch: AppDispatch) => {
-    const block = createBlankSolutionBlock();
+    const componentRow = createBlankComponentRow();
+    const block = createBlankSolutionBlock({
+      components: [componentRow.id],
+    });
+
+    dispatch(ComponentRowModelActions.addComponentRow(componentRow));
     dispatch(SolutionBlockModelActions.addSolutionBlock(block));
     dispatch(DocumentActions.addSolution(block.id));
   };
