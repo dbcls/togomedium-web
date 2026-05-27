b969864379e42c3f8dec87a54b75f5d00eb75779
satoshionoda <satoshionoda@yohak.design>
2026-05-27 20:03:02 +0900

    Revert "Update default API URL to dev environment"
    This reverts commit d9d7c79cf3e696efec22c3230ad72ea6eaf53475.

--- @packages/core/src/network/makeApiUrl.ts
+++ @packages/core/src/network/makeApiUrl.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 const ENV_URL_API = import.meta?.env?.VITE_URL_API;
 export const makeApiUrl = (path: string, getParams?: URLSearchParams) => {
-  const URL_API: string = ENV_URL_API ?? "https://dev.togomedium.org/sparqlist/api/";
+  const URL_API: string = ENV_URL_API ?? "https://togomedium.org/sparqlist/api/";
   if (getParams) {
     return `${URL_API}${path}?${getParams.toString()}`;
   } else {
